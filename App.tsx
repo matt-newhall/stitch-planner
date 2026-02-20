b18734b431a61346f1434c3d842ada13c79ada99
matt-newhall <matt@newhall.dev>
2026-02-20 13:40:54 +0000

    feat: navbar colour

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { DarkTheme, NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
 import { ActivityIndicator, View } from 'react-native'
@@ -55,12 +56,14 @@ const App = () => {
   }
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer theme={navTheme}>
-        <RootNavigator />
-        <StatusBar style="light" />
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NavigationContainer theme={navTheme}>
+          <RootNavigator />
+          <StatusBar style="light" />
+        </NavigationContainer>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   )
 }
 
